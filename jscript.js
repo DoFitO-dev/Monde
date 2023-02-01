f82a28bd42b87f790843a1d350103e347dafb2d7
DoFitO-dev <85388937+DoFitO-dev@users.noreply.github.com>
2023-02-01 23:20:59 +0530

    cursor

--- jscript.js
+++ jscript.js
@@ -5,3 +5,10 @@ setTimeout(() => {
   progress.style.opacity = 1;
   progress.style.font.size = 1;
 }, 500)
+
+var cursor = document.querySelector(".cursor");
+var cursor2 = document.querySelector(".cursor2");
+
+document.addEventListener("mousemove", function(e) {
+  cursor.style.cssText = cursor2.style.cssText = "left:" + e.clientX + "px; top: " + e.clientY + "px;";
+})
