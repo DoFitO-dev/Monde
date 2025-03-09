8726b470e7f4fb635be3ff2b5d4be29d854f962f
DoFitO-dev <hifiwifi144@gmail.com>
2025-03-09 22:33:33 +0530

    bigup

--- jscript.js
+++ jscript.js
@@ -1,22 +1,16 @@
-const progress = document.querySelector('.progress-done');
-
-setTimeout(() => {
-  progress.style.width = progress.getAttribute('data-done') + '%';
-  progress.style.opacity = 1;
-  progress.style.font.size = 1;
-}, 500)
-
-var cursor = document.querySelector(".cursor");
-var cursor2 = document.querySelector(".cursor2");
-
-document.addEventListener("mousemove", function(e) {
-  cursor.style.cssText = cursor2.style.cssText = "left:" + e.clientX + "px; top: " + e.clientY + "px;";
-})
-
-var menutgl = document.querySelector(".menu-toggler");
-var nav = document.querySelector(".nav-links");
-
-menutgl.addEventListener("click", () => {
-  menutgl.classList.toggle("active");
-  nav.classList.toggle("active");
+let cards = document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('mousemove', (e) => {
+        let x = e.offsetX;
+        let y = e.offsetY;
+        let cardWidth = card.clientWidth;
+        let cardHeight = card.clientHeight;
+        let transX = (x - cardHeight/2);
+        let transY = (y - cardHeight/2);
+        card.style.transform = `translateX(${transX}px) translateY(${transY}px)`;
+        card.style.boxShadow = `${transX/-1}px ${transY/-1}px 10px #fff`;
+    })
+    card.addEventListener('mouseout', (e) => {
+        card.style.transform = ``;
+        card.style.boxShadow = ``;
+    })
 })
