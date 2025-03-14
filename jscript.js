9688245bfcd4182093d96c1b2ee0536e14b465f6
DoFitO-dev <hifiwifi144@gmail.com>
2025-03-14 21:36:44 +0530

    back to old landing page

--- jscript.js
+++ jscript.js
@@ -1,3 +1,11 @@
+const progress = document.querySelector('.progress-done');
+ 
+ setTimeout(() => {
+   progress.style.width = progress.getAttribute('data-done') + '%';
+   progress.style.opacity = 1;
+   progress.style.font.size = 1;
+ }, 500)
+
 let cards = document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('mousemove', (e) => {
         let x = e.offsetX;
@@ -7,7 +15,7 @@ let cards = document.querySelectorAll('.card').forEach(card => {
         let transX = (x - cardHeight/2);
         let transY = (y - cardHeight/2);
         card.style.transform = `translateX(${transX}px) translateY(${transY}px)`;
-        card.style.boxShadow = `${transX/-1}px ${transY/-1}px 10px #fff`;
+        card.style.boxShadow = `${transX/-1}px ${transY/-1}px 10px #9d84ff`;
     })
     card.addEventListener('mouseout', (e) => {
         card.style.transform = ``;
